36e8573d47c9d51bbc97a586f0d37efa825a5f9b
Massimo Zaniboni <massimo.zaniboni@asterisell.com>
2015-10-17 17:06:28 +0200

    Improved API of test robot.

--- src/Game/Netrobots/Examples/TestRobotClassic.hs
+++ src/Game/Netrobots/Examples/TestRobotClassic.hs
@@ -43,9 +43,9 @@ type Direction = Int
 -- | Distance from 0 to 1000
 type Distance = Int
 
-{-  XXX 
+{- XXX
 -- | Create a Robot with default params.
-initRobot :: ConnectionConfiguration -> IO (, RobotToken)
+initRobot :: ConnectionConfiguration -> ZMQ z (RobotSocket w, RobotToken)
 initRobot connConf = runZMQ $ do
   s <- socket Req
   connect s (gameServerAddress connConf)
@@ -57,8 +57,8 @@ initRobot connConf = runZMQ $ do
   st <- sendMainCmd s cmd
   let tok = Status.token st
   return (s, tok)
- XXX -}
-                
+-}
+
 createCmd_fire :: Direction -> Distance -> Cannon
 createCmd_fire dir dst
   = Cannon { Cannon.direction = fromIntegral $ dir, Cannon.distance = fromIntegral $ dst }
@@ -79,9 +79,11 @@ createCmd_scan dir ap
 -- | Robot token identifying it in the system.
 type RobotToken = Utf8
 
+type RobotSocket z = Socket z Req
+
 -- | Send a command to the server and wait for the answer with the new robot status.
 --   A robot can move, scan and fire (if it is not reloading) at each move.
-sendCmd :: Socket z Req -> RobotToken -> Maybe Drive -> Maybe Scan -> Maybe Cannon -> ZMQ z RobotStatus
+sendCmd :: RobotSocket z -> RobotToken -> Maybe Drive -> Maybe Scan -> Maybe Cannon -> ZMQ z RobotStatus
 sendCmd s tok md ms mc
   = do let mainCmd = MainCommand {
                          createRobot = Nothing
@@ -95,11 +97,13 @@ sendCmd s tok md ms mc
                          , deleteRobot = Nothing
                          }
        sendMainCmd s mainCmd
-       
-waitStatus :: Socket z Req -> RobotToken -> ZMQ z RobotStatus 
+
+-- | Do nothing, and wait for the status.
+waitStatus :: RobotSocket z -> RobotToken -> ZMQ z RobotStatus 
 waitStatus s tok = sendCmd s tok Nothing Nothing Nothing
 
-sendMainCmd :: Socket z Req -> MainCommand -> ZMQ z RobotStatus
+-- | Internal/private function.
+sendMainCmd :: RobotSocket z -> MainCommand -> ZMQ z RobotStatus
 sendMainCmd s cmd
     = do liftIO $ putStrLn $ "Send command: " ++ show cmd
          let protoCmd = LBS.toStrict $ messagePut cmd
@@ -109,7 +113,40 @@ sendMainCmd s cmd
          liftIO $ putStrLn $ "Robot status: " ++ show status ++ "\n"
          return status
 
+--
+-- Tasks
+--
 
+task_gotoPosition :: RobotSocket z -> RobotToken -> Point -> ZMQ z ()
+task_gotoPosition s tok (x1, y1)
+    = do s1 <- waitStatus s tok
+         let (x0, y0) = (fromIntegral $ x s1, fromIntegral $ y s1)
+         let (Angle.Degrees dg) = Angle.degrees $ Angle.Radians $ atan2 (fromIntegral $ y1 - y0) (fromIntegral $ x1 - x0)
+         let heading = fromInteger $ toInteger $ round dg 
+         let driveCmd = createCmd_drive heading 100
+         s2 <- sendCmd s tok (Just driveCmd) Nothing Nothing
+         task_waitNearPosition s tok (x1, y1)
+         _ <- sendCmd s tok (Just $ createCmd_drive heading 0) Nothing Nothing
+         task_waitStopMovement s tok
+         return ()
+
+task_waitNearPosition s tok (x1, y1)
+    = do s1 <- waitStatus s tok
+         let (x0, y0) = (fromIntegral $ x s1, fromIntegral $ y s1)
+         let d1 = point_distance (x0, y0) (x1, y1)
+         case d1 > 80.0 of
+           True -> task_waitNearPosition s tok (x1, y1)
+           False -> return ()
+           
+task_waitStopMovement s tok 
+   = do s1 <- waitStatus s tok 
+        case (Status.speed s1) > 0 of
+          True -> task_waitStopMovement s tok
+          False -> return ()
+ 
+
+
+-- | An example of Robot.
 robotClassic :: ConnectionConfiguration -> IO ()
 robotClassic connConf = runZMQ $ do
   s <- socket Req
@@ -122,38 +159,9 @@ robotClassic connConf = runZMQ $ do
   st <- sendMainCmd s cmd
   let tok = Status.token st
 
-  gotoPosition s tok (100,200) 
-
+  task_gotoPosition s tok (100,200) 
   return ()
- where
-
-  
-  gotoPosition s tok (x1, y1)
-    = do s1 <- waitStatus s tok
-         let (x0, y0) = (fromIntegral $ x s1, fromIntegral $ y s1)
-         let (Angle.Degrees dg) = Angle.degrees $ Angle.Radians $ atan2 (fromIntegral $ y1 - y0) (fromIntegral $ x1 - x0)
-         let heading = fromInteger $ toInteger $ round dg 
-         let driveCmd = createCmd_drive heading 100
-         s2 <- sendCmd s tok (Just driveCmd) Nothing Nothing
-         waitNearPosition s tok (x1, y1)
-         _ <- sendCmd s tok (Just $ createCmd_drive heading 0) Nothing Nothing
-         waitStopMovement s tok
-         return ()
-
-  waitNearPosition s tok (x1, y1)
-    = do s1 <- waitStatus s tok
-         let (x0, y0) = (fromIntegral $ x s1, fromIntegral $ y s1)
-         let d1 = point_distance (x0, y0) (x1, y1)
-         case d1 > 80.0 of
-           True -> waitNearPosition s tok (x1, y1)
-           False -> return ()
-           
-  waitStopMovement s tok 
-   = do s1 <- waitStatus s tok 
-        case (Status.speed s1) > 0 of
-          True -> waitStopMovement s tok
-          False -> return ()
-          
+         
 fromProtoBuffer :: (ReflectDescriptor msg, Wire msg) => ByteString -> msg
 fromProtoBuffer bs
   = case messageGet bs of
